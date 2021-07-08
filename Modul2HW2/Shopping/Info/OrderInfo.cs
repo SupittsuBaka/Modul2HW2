e706f17897ec50f79c9bc38fe949b14c50f1a69c
Admin-PC\Admin <sashashmidt02@gmail.com>
2021-07-08 20:50:58 +0300

    Итоговый проект

---
+++ Modul2HW2/Shopping/Info/OrderInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Shopping
+{
+    public class OrderInfo
+    {
+        public int OrderId { get; set; }
+        public int OrderSum { get; set; }
+        public User User { get; set; }
+    }
+}
